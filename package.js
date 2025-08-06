42b94a22dd2bd3f95e4030bb9734c484ba11be43
TARZAN-XD <tkrshqtk@gmail.com>
2025-08-06 15:06:22 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "video-downloader",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js",
+    "install-backend": "cd backend && npm install"
+  },
+  "dependencies": {
+    "axios": "^1.6.7",
+    "body-parser": "^1.20.2",
+    "cors": "^2.8.5",
+    "express": "^4.19.2"
+  }
+}
